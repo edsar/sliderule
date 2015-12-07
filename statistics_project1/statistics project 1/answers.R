b551395204df55591cc874f469400a69165ea0f9
edsarausad <sarausad@gmail.com>
2015-12-06 20:01:49 -0800

    homework working on 3

--- statistics_project1/statistics project 1/answers.R
+++ statistics_project1/statistics project 1/answers.R
@@ -1,14 +1,26 @@
+#
+# Statistics Project 1
+#
+# Ed Sarausad
+# 12/6/2015
+#
+# TODO: Consider use of FWER, FDR, and associated solutions
+#
 library(stats)
 library(utils)
 library(nortest)
 
+# User inference function provide by DataCamp
+load(url("http://assets.datacamp.com/course/dasi/inference.Rdata"))
 setwd("~/sliderule/statistics_project1/statistics project 1")
 
 df <- read.csv("data/human_body_temperature.csv")
 
 qqnorm(df$temperature)
 
-# run tests of null that distribution is normal
+# 1.Is the distribution of body temperatures normal? .Remember that this is a condition for the CLT, and hence the statistical tests we are using, to apply. 
+#
+# First, we'll run tests of null that distribution is normal using normality package, nortest
 ad.test(df$temperature)
 # A = 0.5201, p-value = 0.1829
 
@@ -24,3 +36,27 @@ pearson.test(df$temperature)
 
 sf.test(df$temperature)
 # W = 0.98379, p-value = 0.1113
+
+# 2.Is the true population mean really 98.6 degrees
+# Bring out the one sample hypothesis test! In this situation, is it approriate to apply a z-test or a t-test? How will the result be different?
+inference(df$temperature, est = "mean", method = "theoretical", type = "ci", conflevel = 0.95, boot_method = "perc")
+# At 95% CI, 98.6 does not fall within the CI
+
+# 3.At what temperature should we consider someone's temperature to be "abnormal"?.Start by computing the margin of error and confidence interval.
+# According to this sample, an abnormal temperature at 99% CI would be less than 98.0836 or greater than 98.4149
+inference(df$temperature, est = "mean", method = "theoretical", type = "ci", conflevel = 0.99, boot_method = "perc")
+
+# 4.Is there a significant difference between males and females in normal temperature?.Set up and solve for a two sample hypothesis testing.
+# H0 : There is no difference between male and female normal temperatures
+# HA : There is significant different between male and female normal temperatures
+male <- subset(df, df$gender=="M")
+female <- subset(df, df$gender=="F")
+#inference(male, female, est = "proportion", method = "theoretical", type = "ht", conflevel = 0.95, boot_method="perc", alternative = "twosided")
+# Pooled T-test
+t.test(female$temperature, male$temperature, alternative = "less", mu=0, var.equal = TRUE)
+t.test(female$temperature, male$temperature, alternative = "less", mu=0)
+
+# p-value is much lower (closer to significant result) when male temp is response variable -- what does this mean?
+t.test(male$temperature, female$temperature, alternative = "less", mu=0, var.equal = TRUE)
+t.test(male$temperature, female$temperature, alternative = "less", mu=0)
+
--- statistics_project2/statistics project 2/answers.R
+++ statistics_project2/statistics project 2/answers.R
@@ -1,24 +1,76 @@
+#
+# Statistics Project 2
+#
+# You will perform a statistical analysis to establish whether race has a significant impact on the rate of callbacks for resumes.
+
+# Parameter of interest:  difference in callback rates in the population at large between black and white applicants
+# Point estimate:         difference in callback rates in the sample of black and white applicants
+
+# Answer the following questions in this notebook below and submit to your Github account. 
+# 1.What test is appropriate for this problem? Does CLT apply
+# 2.What are the null and alternate hypotheses?
+# 3.Compute margin of error, confidence interval, and p-value.
+# 4.Discuss statistical significance.
+#
+# Resources: 
+# http://www.stat.columbia.edu/~martin/W2024/R2.pdf
+# https://stat.ethz.ch/R-manual/R-devel/library/stats/html/p.adjust.html
+#
+#
+
 library(stats)
 library(foreign)
 
+# load the data
 setwd("~/sliderule/statistics_project2/statistics project 2")
-
 data <- read.dta('data/us_job_market_discrimination.dta')
 
-black <- data[data$race=="b",]
-white <- data[data$race=="w",]
+# Use inference function provide by DataCamp
+load(url("http://assets.datacamp.com/course/dasi/inference.Rdata"))
 
-t.test(black$call, white$call)
+# clean data
+data$call <- as.logical(data$call)
 
-# 
-# Welch Two Sample t-test
-# 
-# data:  black$call and white$call
-# t = -4.1147, df = 4711.6, p-value = 3.943e-05
-# alternative hypothesis: true difference in means is not equal to 0
-# 95 percent confidence interval:
-#   -0.04729503 -0.01677067
-# sample estimates:
-#   mean of x  mean of y 
-# 0.06447639 0.09650924 
+# subset for comparison
+black <- subset(data, data$race=="b")
+white <- subset(data, data$race=="w")
+
+# 1.What test is appropriate for this problem? Does CLT apply
+# check condition: samples are independent (assume yes)
+# check condition: 10% population (total population of black/white applicants in country is >10x of sample)
+
+# check condition: success and failure cases >10 each (yes)
+nrow(black)
+nrow(white)
+# same number of rows: 2435
+
+# 2.What are the null and alternate hypotheses?
+# H0 = there is no difference between callback rates on resumes from blacks vs. whites 
+# HA = there is a difference between callback rates
 #
+# Conclustion: There is a significant difference in callback rates, reject null
+#
+# TODO: Contract t.test() with inference()
+
+t.test(black$call, white$call, alternative = "less", mu = 0, var.equal = TRUE)
+t.test(black$call, white$call, alternative = "less", mu = 0)
+
+# 3.Compute margin of error, confidence interval, and p-value.
+#
+# Conclusion: At 95% CI, callback rate to black applicants is lower by 23.08 to 35.18%
+#
+# TODO: Can we do multiple tests and use p-adjust to deal with FWER and FDR? 
+
+inference(black$call, white$call, est = "proportion", type = "ci", conflevel = 0.95, boot_method = "perc", method = "theoretical", success = TRUE)
+
+# 4.Discuss statistical significance.
+#
+# Alternative hypothesis supported that there is a difference in callback rates at 95% CI
+#
+# Conclusion: 
+#
+# TODO: Can we do multiple tests and use p-adjust to deal with FWER and FDR?
+
+inference(black$call, white$call, est = "proportion", type = "ht", conflevel = 0.95, boot_method = "perc", method = "theoretical", alternative = "less", success = TRUE, null = 0)
+
+
