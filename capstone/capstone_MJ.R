7f46b040a4278d9ccccef5b2aef68e826a411eed
edsarausad <ed@alacergroup.com>
2016-01-11 17:34:54 -0800

    update

--- capstone/capstone_MJ.R
+++ capstone/capstone_MJ.R
@@ -1,18 +1,31 @@
 library(broom) # used to neatly pass model results (model chaining/iteration)
-
+library(forecast) #consider using plot.ly for interactive forecast
 
 sales <- read.csv("https://www.dropbox.com/s/yxgij2mtsou0lf0/mj-sales-transformed.csv?dl=1")
 violations <- read.csv("https://www.dropbox.com/s/euwbe6mciu9oaw9/mj-violations-transformed.csv?dl=1")
 applicants <- read.csv("https://www.dropbox.com/s/hw3ci1l8w55wwxw/all_applicants.csv?dl=1")
+applicants$License_Number <- applicants$License.
 
-# why does this not come to a flat 25% rate for all?
-sales$tax_rate <- as.numeric(sales$Excise_Tax_Due) / as.numeric(sales$Total_Sales)
+# why does this not come to a flat 25% rate for all? because as.numeric converts the value incorrectly?!
+sales$Total_Sales <- as.numeric(gsub("\\$","", gsub("\\,","", sales$Total_Sales))) 
+sales$Excise_Tax_Due <- as.numeric(gsub("\\$","", gsub("\\,","", sales$Excise_Tax_Due))) 
+sales$tax_rate <- sales$Excise_Tax_Due / sales$Total_Sales
 
 # use applicants and sum sales and violations
+# to get: license_number | total_sales | total_violations
+# TODO: Do I need to scale features?
 
+# get some aggregates first
+salesonly <- sales[,c(2,4)]
+aggsales <- aggregate(salesonly, by=list(sales$License_Number), FUN=sum)
+
+# TODO no count??
+# aggviolations <- aggregate(violations, by=list(violations$License_Number), FUN=count)
+
+# then merge on license_number
+applicants_with_sales <- merge(applicants, aggsales, by="License_Number")
 
 # inference and t-test?
 load(url("http://assets.datacamp.com/course/dasi/inference.Rdata"))
 tt=t.test()
 
-# regression, clustering
--- capstone/capstone_mj.py
+++ capstone/capstone_mj.py
@@ -10,7 +10,8 @@
 #
 
 import pandas as pd
-from sklearn import cross_validation
+from sklearn import tree
+from sklearn.naive_bayes import GaussianNB 
 from __future__ import division
 
 # load data
@@ -34,17 +35,12 @@ medicals["type"] = "medical"
 all_dfs = [producers, processors, retailers, medicals]
 all_applicants = pd.concat(all_dfs)
 
-# use applicants and sum sales and violations
+# use applicants file and sum sales and violations in new columns
 from re import sub
 from decimal import Decimal
 sales['Total_Sales_Num'] = Decimal(sub(r'[^\d.]', '', sales['Total_Sales'])) 
 by_license = sales.groupby("License_Number")
 by_license = violations.groupby("License_Number").count()
-
-
-# partition training/test 50/50
-# work on training only (including CV, don't touch test)
-features_train, features_test, labels_train, labels_test = cross_valiation.train_test_split(
 
 # transform data
 # question: Are there anomalies that we should clean up? (outlier detection) - with plots, not extensive since low # fields
@@ -61,13 +57,18 @@ print len(violaters.index)/len(all_applicants.index) *100 , " % violators out of
 # answer: <4%
 
 
+# partition training/test 50/50, consider 60/20/20?
+# work on training only (including CV, don't touch test)
+from sklearn import cross_validation
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(
+
 
 # question: Can businesses be plotted into an interesting 2x2? (scatterplot, unsup clustering/classification, PCA?)
 # question: How do violations impact sales? (survival analysis) *interpretive, yield odds of dying from illness, dropout rates* (LOW-PRI)
 # question: Are there features that predict violations? (regression/classification) *more iterations of predictive model*
 
 
-# expore data
+# explore data
 # question: Does the learning model exhibit bias or variance?
 # question: Are there outliers/anomalies that we shouldn't ignore?
 
@@ -78,4 +79,4 @@ print len(violaters.index)/len(all_applicants.index) *100 , " % violators out of
 
 # modeling opportunity (for increased banking relationship)
 # question:  Which businesses have positively forecasted sales? (R probably better-- forecast package)
-# question:  What will be total revenue quarter by quarter? By region?
+# question:  What will be total revenue quarter by quarter or region
